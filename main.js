72d841c5edb3c5e6abf5c7e83fd4b68bd7e474d4
Roohee13 <113611360+Roohee13@users.noreply.github.com>
2024-07-25 15:22:37 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,64 +1,84 @@
-const SHA256 = require('crypto-js/sha256')
+const SHA256 = require("crypto-js/sha256");
+class CryptoBlock {
+  constructor(index, timestamp, data, precedingHash = " ") {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.precedingHash = precedingHash;
+    this.hash = this.computeHash();
+    this.nonce = 0;
+  }
 
-class Block {
-    constructor(timestamp, data) {
-        this.index = 0;
-        this.timestamp = timestamp;
-        this.data = data;
-        this.previousHash = "0";
-        this.hash = this.calculateHash();
-        this.nonce = 0;
+  computeHash() {
+    return SHA256(
+      this.index +
+        this.precedingHash +
+        this.timestamp +
+        JSON.stringify(this.data) +
+        this.nonce
+    ).toString();
+  }
+
+  proofOfWork(difficulty) {
+    while (
+      this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")
+    ) {
+      this.nonce++;
+      this.hash = this.computeHash();
     }
-
-    calculateHash() {
-        return SHA256(this.index + this.previousHash + this.timestamp + this.data + this.nonce).toString();
-    }
-
-    mineBlock(difficulty) {
-
-    }
+  }
 }
 
-class Blockchain{
-    constructor() {
-        this.chain = [this.createGenesis()];
+class CryptoBlockchain {
+  constructor() {
+    this.blockchain = [this.startGenesisBlock()];
+    this.difficulty = 4;
+  }
+  startGenesisBlock() {
+    return new CryptoBlock(0, "01/01/2020", "Initial Block in the Chain", "0");
+  }
+
+  obtainLatestBlock() {
+    return this.blockchain[this.blockchain.length - 1];
+  }
+  addNewBlock(newBlock) {
+    newBlock.precedingHash = this.obtainLatestBlock().hash;
+    //newBlock.hash = newBlock.computeHash();
+    newBlock.proofOfWork(this.difficulty);
+    this.blockchain.push(newBlock);
+  }
+
+  checkChainValidity() {
+    for (let i = 1; i < this.blockchain.length; i++) {
+      const currentBlock = this.blockchain[i];
+      const precedingBlock = this.blockchain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.computeHash()) {
+        return false;
+      }
+      if (currentBlock.precedingHash !== precedingBlock.hash) return false;
     }
-
-    createGenesis() {
-        return new Block(0, "01/01/2017", "Genesis block", "0")
-    }
-
-    latestBlock() {
-        return this.chain[this.chain.length - 1]
-    }
-
-    addBlock(newBlock){
-        newBlock.previousHash = this.latestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
-        this.chain.push(newBlock);
-    }
-
-    checkValid() {
-        for(let i = 1; i < this.chain.length; i++) {
-            const currentBlock = this.chain[i];
-            const previousBlock = this.chain[i - 1];
-
-            if (currentBlock.hash !== currentBlock.calculateHash()) {
-                return false;
-            }
-
-            if (currentBlock.previousHash !== previousBlock.hash) {
-                return false;
-            }
-        }
-
-        return true;
-    }
+    return true;
+  }
 }
 
-let jsChain = new Blockchain();
-jsChain.addBlock(new Block("12/25/2017", {amount: 5}));
-jsChain.addBlock(new Block("12/26/2017", {amount: 10}));
+let smashingCoin = new CryptoBlockchain();
 
-console.log(JSON.stringify(jsChain, null, 4));
-console.log("Is blockchain valid? " + jsChain.checkValid());
+console.log("smashingCoin mining in progress....");
+smashingCoin.addNewBlock(
+  new CryptoBlock(1, "01/06/2020", {
+    sender: "Iris Ljesnjanin",
+    recipient: "Cosima Mielke",
+    quantity: 50
+  })
+);
+
+smashingCoin.addNewBlock(
+  new CryptoBlock(2, "01/07/2020", {
+    sender: "Vitaly Friedman",
+    recipient: "Ricardo Gimenes",
+    quantity: 100
+  })
+);
+
+console.log(JSON.stringify(smashingCoin, null, 4));
